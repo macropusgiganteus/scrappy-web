b8a96eb135ab4e72524f432968103e234a5ea276
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-24 23:19:30 +0700

    feat: add signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users, only: [:index, :new, :create, :authenticate]
 end
