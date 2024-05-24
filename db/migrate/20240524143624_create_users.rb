98e62ed9809706513c912dc21800d3a525a42b4e
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-24 21:38:56 +0700

    fix(db): update user model

--- db/migrate/20240524143624_create_users.rb
+++ db/migrate/20240524143624_create_users.rb
@@ -2,7 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
+      t.string :password_confirmation
       t.datetime :last_signin_at
 
       t.timestamps
