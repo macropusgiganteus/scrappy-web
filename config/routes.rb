79aae46e050b37ffa3d76815ae84c70964dc64cc
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-25 08:17:27 +0700

    fix: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'keywords#index'
   get 'keywords/secret'
-  resources :user_sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:index, :new, :create, :authenticate]
+
+  get 'register', to: 'users#new', as: 'register'
+  get 'login', to: 'user_sessions#new', as: 'login'
+
+  resources :user_sessions, only: [:create, :destroy]
+  resources :users, only: [:index, :create, :authenticate]
 end
