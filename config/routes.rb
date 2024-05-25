307077e568ac6bf9765f7b3accadcd1f5d09388b
Phanupong Inchan <35326900+macropusgiganteus@users.noreply.github.com>
2024-05-25 13:22:23 +0700

    Add authentication. As a user, I can register and login to access private pages (#1)
    * feat(db): add user model
    
    * chore: add devise
    
    * fix(db): update user model
    
    * feat: add signup
    
    * feat: add sign in & sign out
    
    * Revert "chore: add devise"
    
    This reverts commit 8f5c065dbb4412975bd712c5dd831b75bc27dd69.
    
    * fix: update last_signin_at
    
    * chore: add bcrypt
    
    * style: add tailwind style
    
    * chore: remove unused views
    
    * fix: update routes
    
    * feat: add login redirection
    
    * fix: update path
    
    * fix: update button text
    
    * chore: add assets
    
    * style: update login & register page
    
    * style: center body content

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'keywords#index'
+  get 'keywords/secret'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'register', to: 'users#new', as: 'register'
+  get 'login', to: 'user_sessions#new', as: 'login'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :user_sessions, only: [:create, :destroy]
+  resources :users, only: [:index, :create, :authenticate]
 end
