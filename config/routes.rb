ed18107d06cbd81b93c47a4e7b559366198658c9
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-26 23:38:42 +0700

    feat: link keyword action button to html page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'register', to: 'users#new', as: 'register'
   get 'login', to: 'user_sessions#new', as: 'login'
   
+  get '/keywords/:id', to: 'keywords#show', as: 'keyword'
   post 'import_keywords', to: 'keywords#import_csv'
 
   resources :user_sessions, only: [:create, :destroy]
