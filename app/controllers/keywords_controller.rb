3a5461aef4cec53d24b6a92dd5b495992d8b6648
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-26 21:04:45 +0700

    feat: search google for keyword and store to db

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,12 +1,16 @@
 class KeywordsController < ApplicationController
     require 'csv'
+
+    require "httparty" 
+    require "nokogiri"
+
     # Before the action, check if user is logged in.
     before_action :require_login, only: [:index]
 
     # index is a public page
     def index
         if params[:query].present?
-            @keywords = Keyword.where("user_id = ? AND keyword ILIKE ?", session[:user_id], "%#{params[:query]}%")
+            @keywords = Keyword.includes(:keyword_result).where("user_id = ? AND keyword ILIKE ?", session[:user_id], "%#{params[:query]}%")
         else
             @keywords = Keyword.where(user_id: session[:user_id])
         end   
@@ -40,21 +44,61 @@ class KeywordsController < ApplicationController
             CSV.foreach(file.path) do |row|
                 next if Keyword.find_by(user: current_user, keyword: row[0]).present?
                 
-                keyword = Keyword.new
-                keyword.keyword = row[0]
-                keyword.user = current_user
-                
+                keyword = Keyword.new(user: current_user, keyword: row[0])
+
                 if keyword.valid?
-                    keyword.save!
-                    total_created += 1
+                    total_ads, total_links, stats, html = *google_search(keyword.keyword)
+
+                    keyword_result = KeywordResult.new(keyword: keyword, total_ads: total_ads, total_links: total_links, total_search_results: stats, html: html)
+
+                    if keyword_result.valid?
+                        keyword.save!
+                        keyword_result.save!
+                        total_created += 1
+                    else 
+                        redirect_to keywords_path, notice: "kw_res: #{keyword_result}"
+                        return true
+                    end 
+
                 else
                     # Handle invalid rows here, e.g., log errors
                     Rails.logger.error "Invalid row: #{keyword.errors.full_messages}"
                 end
             end
-            redirect_to keywords_path, notice: "CSV imported successfully. #{total_created} new keywords were imported"
+            if total_created == 0
+                redirect_to keywords_path, notice: "No new keywords were found in the CSV file."
+            else 
+                redirect_to keywords_path, notice: "CSV imported successfully. #{total_created} new keywords were imported"
+            end
         rescue CSV::MalformedCSVError => e
             redirect_to keywords_path, alert: "There was an error processing the file: #{e.message}"
         end
     end      
+
+    private 
+
+    def google_search(keyword)
+        response = HTTParty.get("http://www.google.com/search?q=#{keyword}&hl=en", { 
+            headers: { 
+                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36" 
+            }, 
+        })
+
+        # parsing the HTML document returned by the server 
+        document = Nokogiri::HTML(response.body)
+
+        # defining a data structure to store the scraped data 
+        # Result = Struct.new(:url, :image, :name, :price)
+
+        html_ads_section = document.at_css('div#tads[aria-label=Ads]') || document.at_css('div#tads')
+        total_ads = html_ads_section ? html_ads_section.count : 0
+
+        # selecting all HTML link elements 
+        html_links = document.css("a")
+        total_links = html_links.count
+
+        html_stats = document.at_css("div#result-stats")
+
+        return total_ads, total_links, html_stats.content, document
+    end
 end
