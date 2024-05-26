521f56f0495a3ee0603632c239a4e59e3b2c3d86
Phanupong Inchan <35326900+macropusgiganteus@users.noreply.github.com>
2024-05-26 15:52:35 +0700

    Add import keywords from CSV, As a user, I can import new keywords from a CSV file (#3)
    * feature: add import keywords with csv file
    
    * fix: skip duplicated keywords when importing

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,4 +1,5 @@
 class KeywordsController < ApplicationController
+    require 'csv'
     # Before the action, check if user is logged in.
     before_action :require_login, only: [:index]
 
@@ -10,4 +11,50 @@ class KeywordsController < ApplicationController
             @keywords = Keyword.where(user_id: session[:user_id])
         end   
     end
-  end
+
+    def import_csv    
+        if !params[:file].present?
+            redirect_to keywords_path, alert: "Please upload a file"
+            return true
+        end
+
+        file = params[:file]
+
+        if file.content_type != 'text/csv'
+            redirect_to keywords_path, alert: "Invalid file type. Please upload a CSV file."
+            return true
+        end
+
+        total_rows = CSV.read(file.path).count
+        if (total_rows == 0)
+            redirect_to keywords_path, alert: "The file is empty."
+            return true
+        elsif (total_rows > 100)
+            redirect_to keywords_path, alert: "Keywords exceed 100 rows."
+            return true
+        end
+
+        # Process the file
+        total_created = 0
+        begin
+            CSV.foreach(file.path) do |row|
+                next if Keyword.find_by(user: current_user, keyword: row[0]).present?
+                
+                keyword = Keyword.new
+                keyword.keyword = row[0]
+                keyword.user = current_user
+                
+                if keyword.valid?
+                    keyword.save!
+                    total_created += 1
+                else
+                    # Handle invalid rows here, e.g., log errors
+                    Rails.logger.error "Invalid row: #{keyword.errors.full_messages}"
+                end
+            end
+            redirect_to keywords_path, notice: "CSV imported successfully. #{total_created} new keywords were imported"
+        rescue CSV::MalformedCSVError => e
+            redirect_to keywords_path, alert: "There was an error processing the file: #{e.message}"
+        end
+    end      
+end
