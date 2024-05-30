3388c8e4f65d20739328804dcac7179a6fb09a0d
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-31 06:55:36 +0700

    test: add tests - authentication page - authorizing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,18 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
-    assert_response :success
-  end
-
   test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get users_create_url
+    get register_url
     assert_response :success
   end
 end
