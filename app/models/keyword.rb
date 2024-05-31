85ff394cd23d50480767875ba65c6937dd4a86bf
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-31 21:53:58 +0700

    test: add keyword model test

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,5 @@
 class Keyword < ApplicationRecord
   belongs_to :user
   has_one :keyword_result
+  validates :keyword, presence: true
 end
--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class KeywordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @keyword = keywords(:one)
+  end
+
+  test "should be valid" do
+    assert @keyword.valid?
+  end
+
+  test "keyword should be present" do
+    @keyword.keyword = ""
+    assert_not @keyword.valid?
+    assert_includes @keyword.errors[:keyword], "can't be blank"
+  end
+
+  test "should belong to a user" do
+    assert_respond_to @keyword, :user
+  end
+
+  test "should have one keyword result" do 
+    assert_respond_to @keyword, :keyword_result
+  end
 end
