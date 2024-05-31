c583e8eb02ac1982fbca0e423981cc37bc15bad8
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-31 21:54:17 +0700

    test: add keyword result model test

--- app/models/keyword_result.rb
+++ app/models/keyword_result.rb
@@ -1,3 +1,7 @@
 class KeywordResult < ApplicationRecord
   belongs_to :keyword
+  validates :total_ads, presence: true
+  validates :total_links, presence: true
+  validates :total_search_results, presence: true
+  validates :html, presence: true
 end
--- test/models/keyword_result_test.rb
+++ test/models/keyword_result_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class KeywordResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @keyword_result = keyword_results(:one)
+  end
+
+  test "should be valid" do
+    assert @keyword_result.valid?
+  end
+
+  test "total_ads should be present" do 
+    @keyword_result.total_ads = nil
+    assert_not @keyword_result.valid?
+    assert_includes @keyword_result.errors[:total_ads], "can't be blank"
+  end
+
+  test "total_links should be present" do 
+    @keyword_result.total_links = nil
+    assert_not @keyword_result.valid?
+    assert_includes @keyword_result.errors[:total_links], "can't be blank"
+  end
+
+  test "total_search_results should be present" do 
+    @keyword_result.total_search_results = nil
+    assert_not @keyword_result.valid?
+    assert_includes @keyword_result.errors[:total_search_results], "can't be blank"
+  end
+
+  test "html should be present" do 
+    @keyword_result.html = nil
+    assert_not @keyword_result.valid?
+    assert_includes @keyword_result.errors[:html], "can't be blank"
+  end
+
+  test "should belong to a keyword" do
+    assert_respond_to @keyword_result, :keyword
+  end
 end
