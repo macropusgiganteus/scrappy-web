b001e6db5c1f227d20836f1e8d7b0bd8c2d87a7b
Phanupong Inchan <phanupong.inchan67@gmail.com>
2024-05-31 22:39:03 +0700

    test: add keywords controller test

--- test/controllers/keywords_controller_test.rb
+++ test/controllers/keywords_controller_test.rb
@@ -11,6 +11,41 @@ class KeywordsControllerTest < ActionDispatch::IntegrationTest
 
         get keywords_url
         assert_response :success
-        
+    end
+
+    test "user can see keywords list" do
+        # Arrange
+        login()
+
+        # Action
+        get keywords_url
+        assert_response :success
+
+        # Assert
+        html = Nokogiri::HTML(response.body)
+        tbody = html.at_css('tbody')
+        row_count = tbody.css('tr').size
+        assert_equal 5, row_count
+    end
+
+    test "user can filter keywords" do
+        # Arrange
+        login()
+
+        # Action
+        get keywords_url, params: { query: 'keyword2'}
+        assert_response :success
+
+        # Assert
+        html = Nokogiri::HTML(response.body)
+        tbody = html.at_css('tbody')
+        row_count = tbody.css('tr').size
+        assert_equal 3, row_count
+    end
+
+    test "user can upload csv file" do
+    end
+
+    test "fire search google when csv file is uploaded" do
     end
 end
